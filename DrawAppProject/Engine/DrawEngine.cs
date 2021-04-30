23c6997fcf0b500f104f8a69bfafa18b2517cac9
Fahad <82506574+fahad-safcsp@users.noreply.github.com>
2021-04-30 20:59:13 +0300

    Add project files.

---
+++ DrawAppProject/Engine/DrawEngine.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrawAppProject.Engine
+{
+    public class DrawEngine
+    {
+        protected List<Shape> shapes = new List<Shape>();
+
+        public DrawEngine()
+        {
+
+        }
+
+        public DrawEngine ParseTokens(List<Token> tokens)
+        {
+            // parse
+            // set shapes
+            return this;
+        }
+
+        public string GetSourceCode()
+        {
+            // convert shapes to string 
+            return "";
+
+            //StringBuilder sb = new StringBuilder();
+
+            //sb.AppendLine("#rect");
+            //sb.Append("");
+            //sb.Append(":");
+            //sb.Append("Value");
+            //sb.AppendLine("key:value");
+
+            //sb.ToString()
+
+        }
+    }
+}
---
+++ DrawAppProject/Extension/Extension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrawAppProject.Extension
+{
+    public class Extension
+    {
+        public string Load(string path)
+        { return ""; }
+
+        public void Save(string source)
+        { }
+    }
+}
